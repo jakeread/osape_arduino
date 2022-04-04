d8ee682fc7883268ce15a2aa2c5905732a01761a
Jake Read <jake.read@cba.mit.edu>
2022-04-04 11:56:55 -0400

    at basic vbus wire

---
+++ vb_arduinoWire.cpp
@@ -0,0 +1,69 @@
+/*
+arduino-ports/vp_arduinoWire.cpp
+
+turns Wire instances into competent bus link layers for OSAP 
+
+Jake Read at the Center for Bits and Atoms
+(c) Massachusetts Institute of Technology 2022
+
+This work may be reproduced, modified, distributed, performed, and
+displayed for any purpose, but must acknowledge the squidworks and ponyo
+projects. Copyright is retained and must be preserved. The work is provided as
+is; no warranty is provided, and users accept all liability.
+*/
+
+#include "vb_arduinoWire.h"
+#include "../osap_debug.h"
+
+// static stash: same per instance, 
+uint8_t stash[32];
+uint8_t stashLen = 0;
+
+VBus_ArduinoWire::VBus_ArduinoWire(Vertex* _parent, String _name, TwoWire* _wire, uint8_t _ownRxAddr
+) : VBus ( _parent, _name ) {
+  wire = _wire;
+  ownRxAddr = _ownRxAddr;
+}
+
+void VBus_ArduinoWire::begin(void){
+  wire->begin(ownRxAddr);
+  wire->onReceive(this->onRecieve);
+}
+
+void VBus_ArduinoWire::onRecieve(int count){
+  Wire.readBytes(stash, count);
+  stashLen = count;
+}
+
+void VBus_ArduinoWire::loop(void){
+  // check incoming, 
+  if(stashLen > 0){
+    if(stackEmptySlot(this, VT_STACK_ORIGIN)){
+      stackLoadSlot(this, VT_STACK_ORIGIN, stash, stashLen);
+    }
+    stashLen = 0;
+  }
+}
+
+void VBus_ArduinoWire::send(uint8_t* data, uint16_t len, uint8_t rxAddr){
+  digitalWrite(A1, HIGH);
+  // this'll be the big hangup, 
+  if(len > 32) return;
+  // become host, 
+  wire->end();
+  wire->begin();
+  // transmit, 
+  wire->beginTransmission(rxAddr);
+  wire->write(data, len);
+  uint8_t res = wire->endTransmission();
+  // become guest again, 
+  wire->end();
+  wire->begin(ownRxAddr);
+  // check, 
+  if(res != 0) ERROR(2, "res " + String(res));
+  digitalWrite(A1, LOW);
+}
+
+boolean VBus_ArduinoWire::cts(uint8_t rxAddr){
+  return true;
+}
---
+++ vb_arduinoWire.h
@@ -0,0 +1,38 @@
+/*
+arduino-ports/vp_arduinoWire.h
+
+turns Wire instances into competent bus link layers for OSAP 
+
+Jake Read at the Center for Bits and Atoms
+(c) Massachusetts Institute of Technology 2022
+
+This work may be reproduced, modified, distributed, performed, and
+displayed for any purpose, but must acknowledge the squidworks and ponyo
+projects. Copyright is retained and must be preserved. The work is provided as
+is; no warranty is provided, and users accept all liability.
+*/
+
+#ifndef ARDU_WIRELINK_H_
+#define ARDU_WIRELINK_H_
+
+#include <Arduino.h>
+#include <Wire.h>
+#include "../osape/core/vertex.h"
+
+#define WIRELINK_BUFSIZE 255 
+
+class VBus_ArduinoWire : public VBus {
+  public:
+    void begin(void);
+    // -------------------------------- our own loop, cts, and send... 
+    void loop(void) override; 
+    void send(uint8_t* data, uint16_t len, uint8_t rxAddr) override; 
+    boolean cts(uint8_t rxAddr) override; 
+    // -------------------------------- data 
+    TwoWire* wire;
+    static void onRecieve(int count);
+    // -------------------------------- constructors
+    VBus_ArduinoWire(Vertex* _parent, String _name, TwoWire* _wire, uint8_t _ownRxAddr);
+};
+
+#endif 
